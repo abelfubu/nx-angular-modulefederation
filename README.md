8ad4fa162f7763f8760f67306efea485ae58c904
Abel de la Fuente <abelfubu@gmail.com>
2021-09-05 11:56:01 +0200

    feat(chore): add merge webpack with production config

--- README.md
+++ README.md
@@ -17,3 +17,15 @@ npm install @nrwl/angular
 ```bash
 nx g @nrwl/angular:app <host-app-name> --mfe --mfeType=host --routing --style=scss
 ```
+
+### Generate remote App
+
+```bash
+nx g @nrwl/angular:app <remote-app-name> --mfe --mfeType=remote --routing --style=scss --port=4201 --host=<host-app-name>
+```
+
+### Run all Apps
+
+```bash
+nx serve-mfe
+```
--- apps/host1/webpack.prod.config.js
+++ apps/host1/webpack.prod.config.js
@@ -1,1 +1,14 @@
-module.exports = require('./webpack.config');
+const devConfig = require('./webpack.config');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const { merge } = require('webpack-merge');
+
+module.exports = merge(devConfig, {
+  mode: 'production',
+  plugins: [
+    new ModuleFederationPlugin({
+      remotes: {
+        remote1: 'remote1@http://production/remoteEntry.js',
+      },
+    }),
+  ],
+});
